5109e8c07dbd3d4ab227b326190a85cd8b29187c
Faddyk0313 <kingshahbaz0313@gmail.com>
2024-12-09 21:44:48 +0500

    added blogs in aircraft

---
+++ src/components/Airport_Aircraft_Blog_Page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AirportPage from './AirportPage';
+import AircraftPage from './AircraftPage';
+import BlogPage from './BlogPage';
+interface Props {
+    fields: any; // Replace `any` with the actual type of `fields` if known
+    date: string;
+    region: string;
+}
+
+const Airport_Aircraft_Blog_Page = ({ fields, date, region }: Props) => {
+
+    if (region == 'airport_pages') return <AirportPage fields={fields} />;
+    else if (region == 'aircraft_pages') return <AircraftPage fields={fields} />;
+    else if (region == 'blogs') return <BlogPage fields={fields} date={date} />;
+};
+
+export default Airport_Aircraft_Blog_Page;
